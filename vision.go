32c047daaea596a212e8c12b9a2e6a75f3599c84
yyfMichaelYan <yyfmichaelyan@gmail.com>
2020-07-25 17:05:45 +0000

    init commit

---
+++ vision.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	vision "cloud.google.com/go/vision/apiv1"
+)
+
+// Annotate an image file based on Cloud Vision API, return score and error if exists.
+func annotate(uri string) (float32, error) {
+	ctx := context.Background()
+
+	client, err := vision.NewImageAnnotatorClient(ctx)
+	if err != nil {
+		return 0.0, err
+	}
+	defer client.Close()
+
+	image := vision.NewImageFromURI(uri)
+	faceAnnotations, err := client.DetectFaces(ctx, image, nil, 1)
+	if err != nil {
+		return 0.0, err
+	}
+
+	if len(faceAnnotations) == 0 {
+		fmt.Println("No faces found.")
+		return 0.0, nil
+	}
+	return faceAnnotations[0].DetectionConfidence, nil
+}
